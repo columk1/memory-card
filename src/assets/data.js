98accc8aeb01d3bb461f5e659dcad84fc1186cc0
Colum Kelly <columk1@users.noreply.github.com>
2023-10-26 12:53:29 -0700

    Fix: build image links not working

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,18 +1,35 @@
+import imgUrl0 from '/src/assets/card-0.png'
+import imgUrl1 from '/src/assets/card-1.png'
+import imgUrl2 from '/src/assets/card-2.png'
+import imgUrl3 from '/src/assets/card-3.png'
+import imgUrl4 from '/src/assets/card-4.png'
+import imgUrl5 from '/src/assets/card-5.png'
+import imgUrl6 from '/src/assets/card-6.png'
+import imgUrl7 from '/src/assets/card-7.png'
+import imgUrl8 from '/src/assets/card-8.png'
+import imgUrl9 from '/src/assets/card-9.png'
+import imgUrl10 from '/src/assets/card-10.png'
+import imgUrl11 from '/src/assets/card-11.png'
+import imgUrl12 from '/src/assets/card-12.png'
+import imgUrl13 from '/src/assets/card-13.png'
+import imgUrl14 from '/src/assets/card-14.png'
+import imgUrl15 from '/src/assets/card-15.png'
+
 export default [
-  { url: '/src/assets/card-0.png' },
-  { url: '/src/assets/card-1.png' },
-  { url: '/src/assets/card-2.png' },
-  { url: '/src/assets/card-3.png' },
-  { url: '/src/assets/card-4.png' },
-  { url: '/src/assets/card-5.png' },
-  { url: '/src/assets/card-6.png' },
-  { url: '/src/assets/card-7.png' },
-  { url: '/src/assets/card-8.png' },
-  { url: '/src/assets/card-9.png' },
-  { url: '/src/assets/card-10.png' },
-  { url: '/src/assets/card-11.png' },
-  { url: '/src/assets/card-12.png' },
-  { url: '/src/assets/card-13.png' },
-  { url: '/src/assets/card-14.png' },
-  { url: '/src/assets/card-15.png' },
+  { url: imgUrl0 },
+  { url: imgUrl1 },
+  { url: imgUrl2 },
+  { url: imgUrl3 },
+  { url: imgUrl4 },
+  { url: imgUrl5 },
+  { url: imgUrl6 },
+  { url: imgUrl7 },
+  { url: imgUrl8 },
+  { url: imgUrl9 },
+  { url: imgUrl10 },
+  { url: imgUrl11 },
+  { url: imgUrl12 },
+  { url: imgUrl13 },
+  { url: imgUrl14 },
+  { url: imgUrl15 },
 ]
